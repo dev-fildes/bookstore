90b4f2319e7267dc749ec496684c93c93ba1501b
dev-fildes <devfildes@gmail.com>
2022-05-24 04:40:38 -0400

    - Fixed navigation errors

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navigation from "./Navigation";
 import PromotionBar from "./PromotionBar";
-import Home from "./Home";
 import Footer from "./subcomponents/Footer";
 
 export const Store = () => {
