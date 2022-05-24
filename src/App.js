03ef7194af9ad67d3fa7c9d3a8f25e0f34bc2e26
dev-fildes <devfildes@gmail.com>
2022-05-23 23:39:35 -0400

    - Deployment adjustments - Fixed chrome console errors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Index from './components/Index';
 import About from './components/About';
 
 const App = () => {
     return (
         <div className="App">
-                <Router>
-                    <Route exact path="/bookstore" component={Index}/>
-                    <Route exact path="/bookstore/about" component={About}/>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Index/>}/>
+                    <Route path="/bookstore/about" element={<About/>}/>
                     {/*<Route path="/contact" element={<Contact/>}/>*/}
-                </Router>
+                </Routes>
+            </Router>
         </div>
     );
 }
