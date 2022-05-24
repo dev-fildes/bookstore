ab1f46b1e1a7026d43d6287061aa026fb2247f91
dev-fildes <devfildes@gmail.com>
2022-05-24 03:51:17 -0400

    - Fixed navigation errors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/bookstore" element={<Index />}>
+                <Route path="/" element={<Index />}>
                     <Route path="home" element={<Home/>}/>
                     <Route path="store" element={<Store/>}/>
                 </Route>
